4b26fa84d587df1d8845915e01be6443aa4cbeca
Brian Michel <brian.michel@gmail.com>
2012-11-21 19:15:16 -0500

    Initial Commit

---
+++ BMPopover/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BMPopover
+//
+//  Created by Brian Michel on 11/21/12.
+//  Copyright (c) 2012 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BMPopover/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BMPopover
+//
+//  Created by Brian Michel on 11/21/12.
+//  Copyright (c) 2012 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
